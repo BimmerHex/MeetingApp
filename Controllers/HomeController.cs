55bee56676f9a0056f0e40bda52c575c5e8bcb10
BimmerHex <github@vintagegamestudio.com>
2024-02-19 06:59:17 +0300

    ViewBag ve ViewData kullanımları eklendi.

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -9,6 +9,14 @@ namespace MeetingApp.Controllers
         // localhost/home/index
         public IActionResult Index()
         {
+            int hour = DateTime.Now.Hour;
+
+            // ViewBag.WelcomeMessage = hour > 12 ? "İyi Günler" : "Günaydın";
+            // ViewBag.Username = "Gökhan";
+
+            ViewData["WelcomeMessage"] = hour > 12 ? "İyi Günler" : "Günaydın";
+            ViewData["Username"] = "Gökhan";
+
             return View();
         }
     }
